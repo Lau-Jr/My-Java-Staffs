cbba8476fe3590b7dd20cb0b0321a370ddef7903
Laurent Wambura <laurentdeusdedith74@gmail.com>
2025-12-03 13:42:40 +0300

    First Commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+	
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Please Enter Your name: ");
+		String name = input.nextLine();
+		System.out.printf("%nwelcome Mr %s%n",name);
+		}
+}
