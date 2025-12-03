452023e4df00acf5c68a21b8fe38a46e563a0e27
Laurent Wambura <laurentdeusdedith74@gmail.com>
2025-12-03 14:02:15 +0300

    Added Matrix Multiplication Blueprint

---
+++ MatMul.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class MatMul {
+	
+	public double[] multiplyMatrix(double[] A, double[] B){
+		
+		}
+	
+	public static void main(String[] args){
+		MatMul mm = new MatMul();
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		}
+	
+	}
