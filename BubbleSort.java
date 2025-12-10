1df08a2833c8f5b43b84a022cc503954c2fdbf33
LaurentDeus <laurentdeusdedith75@gmail.com>
2025-12-10 16:07:00 +0300

    implemented Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,7 +2,24 @@ public class Sort{
 	
 	private static double[] bubbleSort(double[] data){
 		
-		return new double[]{};
+		int dataLength = data.length;
+		
+		for(int i = 0; i < dataLength - 1; i++){
+			
+			for(int j = i; j < dataLength - 1 - i; j++ ){
+				
+				if (data[j] < data[j+1]){
+					
+					double temp = data[j];
+					data[j] = data[j+1];
+					data[j+1] = temp;
+					
+				}
+				
+			}
+		}
+		
+		return data;
 	}
 
 	private static double[] selectionSort(double[] data){
@@ -21,11 +38,32 @@ public class Sort{
 			return selectionSort(data);
 			
 		else
-			throw new Exception("unsupported Sorting Algorithm");		
+			throw new Exception("unsupported Sorting Algorithm: " + algorithm);		
 
 	}
 	
 	public static void main(String[] args){
+		
+		double[] numbers = {5.2, 3.1, 4.7, 2.9, 1.0};
+		
+		try{
+			sort("bubbe sort", numbers);
+		
+			System.out.println();
+			
+			for (double num : numbers) {
+				System.out.print(num + " ");
+			}
+			
+			System.out.println();
+		}
+		
+		catch(Exception e){
+			
+			System.out.printf("%nException: %s",e.getMessage());
+		}
+		
+		
 		
 	}
 	
